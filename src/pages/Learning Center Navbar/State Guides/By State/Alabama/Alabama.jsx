3bad7f322e2d6ebf34bca4d0109d231fef49988c
Raj-Stha <rajcrestha00@gmail.com>
2024-11-25 10:41:41 +0545

    UI FIX:: State Pages

--- src/pages/Learning Center Navbar/State Guides/By State/Alabama/Alabama.jsx
+++ src/pages/Learning Center Navbar/State Guides/By State/Alabama/Alabama.jsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./Alabama.css";
-import Tabs_Alabama from "./Tabs_Alabama/Tabs_Alabama"
+import Tabs_Alabama from "./Tabs_Alabama/Tabs_Alabama";
+import SideBarBox from "../../../../../components/sideBarBox/SideBarBox";
 
 const Alabama = () => {
   return (
-    <div className='Alabama_main_container'>
-      <div className='top_main_hero_section_Alabama'>
-        <img src="/img/hero-image-map-compass_usa.jpg" alt="" className='top_Alabama_image' />
-        <p className='image_over_paragraph_Alabama'>
-        Alabama
-        </p>
+    <div className="Alabama_main_container">
+      <div className="top_main_hero_section_Alabama">
+        <img
+          src="/img/hero-image-map-compass_usa.jpg"
+          alt=""
+          className="top_Alabama_image"
+        />
+        <p className="image_over_paragraph_Alabama">Alabama</p>
       </div>
 
       <div className="bottom_Alabama">
         <div className="left_side_Alabama">
-          <Tabs_Alabama/>
+          <Tabs_Alabama />
         </div>
-        <div className="right_side_Alabama">
+
+        {/* <div className="right_side_Alabama">
           <h3 className='right_h3_Alabama'>Let's Ascend Incoperate</h3>
           <p>Create your corporation or limited liability company today.</p>
           <button className="Alabama_button_full">Get Started</button>
-        </div>
+        </div> */}
+
+        <SideBarBox
+          heading={"Let's Ascend Incoperate"}
+          content={
+            "Create your corporation or limited liability company today."
+          }
+          sub_content={""}
+          bttnLabel={"Get Started"}
+          bttnLink={"/quote-pricing"}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default Alabama;
--- src/pages/Learning Center Navbar/State Guides/By State/Alaska/Alaska.jsx
+++ src/pages/Learning Center Navbar/State Guides/By State/Alaska/Alaska.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React from "react";
 import "./Alaska.css";
-import Tabs_Alaska from "./Tabs_Alaska/Tabs_Alaska"
+import Tabs_Alaska from "./Tabs_Alaska/Tabs_Alaska";
+import SideBarBox from "../../../../../components/sideBarBox/SideBarBox";
 
 const Alaska = () => {
   return (
-    <div className='Alaska_main_container'>
-      <div className='top_main_hero_section_Alaska'>
-        <img src="/img/hero-image-map-compass_usa.jpg" alt="" className='top_Alaska_image' />
-        <p className='image_over_paragraph_Alaska'>
-        Alaska
-        </p>
+    <div className="Alaska_main_container">
+      <div className="top_main_hero_section_Alaska">
+        <img
+          src="/img/hero-image-map-compass_usa.jpg"
+          alt=""
+          className="top_Alaska_image"
+        />
+        <p className="image_over_paragraph_Alaska">Alaska</p>
       </div>
 
       <div className="bottom_Alaska">
         <div className="left_side_Alaska">
-          <Tabs_Alaska/>
+          <Tabs_Alaska />
         </div>
-        <div className="right_side_Alaska">
+        {/* <div className="right_side_Alaska">
           <h3 className='right_h3_Alaska'>Let's Ascend Incoperate</h3>
           <p>Create your corporation or limited liability company today.</p>
           <button className="Alaska_button_full">Get Started</button>
-        </div>
+        </div> */}
+
+        <SideBarBox
+          heading={"Let's Ascend Incoperate"}
+          content={
+            "Create your corporation or limited liability company today."
+          }
+          bttnLabel={"Get Started"}
+          bttnLink={"/quote-pricing"}
+        />
       </div>
     </div>
   );
-}
+};
 
 export default Alaska;
